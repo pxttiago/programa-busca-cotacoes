3787eb1b49bd7b692abb80496724dbc93a4e27e3
Tiago Peixoto <tspxttech@gmail.com>
2023-02-25 14:08:42 -0300

    Criação das funções do programa
    Criadas as funções de busca de cotações individuais e múltiplas

--- SistemaCotacao.py
+++ SistemaCotacao.py
@@ -1,10 +1,82 @@
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+import requests
+from datetime import datetime
+import numpy as np
 
+
+# lista de moedas com awesome API
+r = requests.get('https://economia.awesomeapi.com.br/json/all')
+dict_r = r.json()
+lista_moedas = list(dict_r.keys())
+
+
+# função pegar cotação
+def pegar_cotacao():
+    moeda = combobox_selecionarmoeda.get()
+    data_cotacao = calendario_moeda.get()
+    data_cotacao_sep = calendario_moeda.get().split('/')
+    dia = data_cotacao_sep[0]
+    mes = data_cotacao_sep[1]
+    ano = data_cotacao_sep[2]
+    link = f'https://economia.awesomeapi.com.br/json/daily/{moeda}-BRL/?' \
+           f'start_date={ano}{mes}{dia}&' \
+           f'end_date={ano}{mes}{dia}'
+    requisicao = requests.get(link)
+    dict_requisicao = requisicao.json()
+    valor_cotacao = dict_requisicao[0]['bid']
+    label_resultadocotacao['text'] = f'A cotação do {moeda} no dia {data_cotacao} foi de: R${valor_cotacao}'
+
+
+# função que abre a janela para seleção do arquivo Excel contendo as moedas que quer puxar a cotação
+def selecionar_arquivo():
+    caminho_arquivo = askopenfilename(title="Selecione um arquivo em excel para abrir")
+    var_caminhoarquivo.set(caminho_arquivo)
+    if caminho_arquivo:
+        label_arquivoselecionado['text'] = f'Arquivo selecionado: {caminho_arquivo}'
+
+
+# função que gera arquivo Excel com as cotações das moedas contidas no arquivo que você selecionou
+def atualizar_cotacoes():
+    df = pd.read_excel(var_caminhoarquivo.get())
+    moedas = df.iloc[:, 0]  # todas as linhas, coluna indice 0
+
+    data_inicial = calendario_datainicial.get()
+    ano_inicial = data_inicial[-4:]
+    mes_inicial = data_inicial[3:5]
+    dia_inicial = data_inicial[:2]
+
+    data_final = calendario_datafinal.get()
+    ano_final = data_final[-4:]
+    mes_final = data_final[3:5]
+    dia_final = data_final[:2]
+
+    for moeda in moedas:
+        link = f'https://economia.awesomeapi.com.br/{moeda}-BRL/100?' \
+            f'start_date={ano_inicial}{mes_inicial}{dia_inicial}&' \
+            f'end_date={ano_final}{mes_final}{dia_final}'
+
+        requisicao_moeda = requests.get(link)
+        cotacoes = requisicao_moeda.json()
+        for cotacao in cotacoes:
+            timestamp = int(cotacao['timestamp'])
+            bid = float(cotacao['bid'])
+            data = datetime.fromtimestamp(timestamp)
+            data = data.strftime('%d/%m/%Y')
+            if data not in df:
+                df[data] = np.nan
+
+            df.loc[df.iloc[:, 0] == moeda, data] = bid
+
+        df.to_excel('Moedas.xlsx')
+        label_atualizarcotacoes['text'] = f'Arquivo atualizado com sucesso'
+
+
+# janela do programa
 janela = tk.Tk()
-
-lista_moedas = ['USD', 'EUR', 'BTC']
 
 janela.title('Sistema de Busca de Cotações de Moedas')
 
@@ -26,14 +98,8 @@ calendario_moeda.grid(row=2, column=2, padx=10, pady=10, sticky='NSWE')
 label_resultadocotacao = tk.Label(text='')
 label_resultadocotacao.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky='NSWE')
 
-
-def pegar_cotacao():
-    pass
-
-
 botao_buscacotacao = tk.Button(text='Buscar Cotação', command=pegar_cotacao)
 botao_buscacotacao.grid(row=3, column=2, padx=10, pady=10, sticky='NSWE')
-
 
 # cotação de múltiplas moedas
 
@@ -43,10 +109,7 @@ label_cotacaovariasmoedas.grid(row=4, column=0, padx=10, pady=10, sticky='NSWE',
 label_selecionararquivo = tk.Label(text='Selecione um arquivo em excel com as moedas na coluna A')
 label_selecionararquivo.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky='NSWE')
 
-
-def selecionar_arquivo():
-    pass
-
+var_caminhoarquivo = tk.StringVar()
 
 botao_selecionararquivo = tk.Button(text='Clique para selecionar', command=selecionar_arquivo)
 botao_selecionararquivo.grid(row=5, column=2, padx=10, pady=10, sticky='NSWE')
@@ -65,11 +128,6 @@ label_datafinal.grid(row=8, column=0, padx=10, pady=10, sticky='NSWE')
 
 calendario_datafinal = DateEntry(year=2023, locale='pt_br')
 calendario_datafinal.grid(row=8, column=1, padx=10, pady=10, sticky='NSWE')
-
-
-def atualizar_cotacoes():
-    pass
-
 
 botao_atualizarcotacoes = tk.Button(text='Atualizar cotações', command=atualizar_cotacoes)
 botao_atualizarcotacoes.grid(row=9, column=0, padx=10, pady=10, sticky='NSWE')
